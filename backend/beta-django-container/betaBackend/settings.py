3dd78a8ccb40734fbea63fe701d0d1cd3c890c08
Trey Plante <treygp15@gmail.com>
2022-06-04 16:33:30 -0500

    DRF reset password

--- backend/beta-django-container/betaBackend/settings.py
+++ backend/beta-django-container/betaBackend/settings.py
@@ -94,6 +94,7 @@ DJOSER = {
     'LOGIN_FIELD': 'email',
     'SEND_ACTIVATION_EMAIL': True,
     'ACTIVATION_URL':'activate/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': 'reset_password/{uid}/{token}',
     'SERIALIZERS': {
         'token_create': 'accounts.serializers.CustomTokenCreateSerializer',
     },
